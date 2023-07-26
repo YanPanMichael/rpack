ac85d9cd530474b4ff81448aaffb42f4455f49b9
YanPanMichael <yanpancsu@icloud.com>
2023-07-26 23:41:36 +0800

    release: v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 All notable changes to this project will be documented in this file. See [chan-log](https://github.com/conventional-changelog/chan-log) for commit guidelines.
 
+## 1.0.0 (2023-07-26)
+
+
+### Features
+
+* init ([588f1c7](https://github.com/YanPanMichael/rpack/commit/588f1c7caca7446f34a3b77920a101c28d4f18ca))
+* save ([155665f](https://github.com/YanPanMichael/rpack/commit/155665f2a9a89cc1c27f8f3d4ab51f1e0414b23c))
+
 ### [1.0.3](https://github.com/YanPanMichael/cpack/compare/v1.0.2...v1.0.3) (2023-07-23)
 
 ### [1.0.2](https://github.com/YanPanMichael/cpack/compare/v1.0.1...v1.0.2) (2023-07-23)
--- dist/rpack.umd.js
+++ dist/rpack.umd.js
@@ -1,7 +1,7 @@
 /*!
 * @autopack/rpack with v0.0.1
 * Author: yanpanmichael
-* Built on 2023-07-25, 00:30:49
+* Built on 2023-07-26, 23:33:29
 * Released under the MIT License Copyright (c) 2023
 */
 !function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t(require("tslib"),require("react")):"function"==typeof define&&define.amd?define(["tslib","react"],t):(e="undefined"!=typeof globalThis?globalThis:e||self).rpack=t(e.tslib,e.React)}(this,(function(e,t){"use strict";return function(n){function l(e){return n.call(this,e)||this}return e.__extends(l,n),l.prototype.render=function(){return t.createElement("div",{className:"some-list"},t.createElement("h1",null,"This is a list for ",this.props.name),t.createElement("ul",null,t.createElement("li",null,"plain javascript"),t.createElement("li",null,"typescript"),t.createElement("li",null,"react"),t.createElement("li",null,"JSX/TSX")))},l}(t.Component)}));//# sourceMappingURL=rpack.umd.map
